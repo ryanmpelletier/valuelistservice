9ab76ff7d062a94c45c3bf586e9967cacb2f7fe0
Pelletier <578993@bah.com>
2016-06-06 14:43:20 -0400

    Add comment about exception.

--- src/main/java/com/pelletier/valuelist/ValueListService.java
+++ src/main/java/com/pelletier/valuelist/ValueListService.java
@@ -10,6 +10,8 @@ import com.pelletier.valuelist.exception.AdapterNotFoundException;
  */
 public interface ValueListService {
 
+	//since my ValueListService implementation might not use an Adapter, it seems odd to have it say
+	//that it throws that specific type of exception
 	public Values<? extends Object> getValuesList(String key, Map<String,Object> queryParams, ValuesInfo valuesInfo) throws AdapterNotFoundException;
 	
 }
