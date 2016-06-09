2ec869ac383b3292289c00413f5086052b635f32
Pelletier <578993@bah.com>
2016-06-09 08:39:48 -0400

    Make DefaultJdbcDataAdapter generic. Also include DefaultRowMapper.

--- src/main/java/com/pelletier/valuelist/App.java
+++ src/main/java/com/pelletier/valuelist/App.java
@@ -31,7 +31,7 @@ public class App {
 			pagingInfo.setNumberPerPage(NUMBER_PER_PAGE);
 			pagingInfo.setPage(PAGE_NUMBER);
 			
-			result = (Values<Map<String,Object>>) valueListService.getValuesList("query2", queryParams,pagingInfo);
+			result = (Values<Map<String,Object>>) valueListService.getValuesList("query", queryParams,pagingInfo);
 			System.out.println(result);
 			
 		} catch (RuntimeException e) {	
