acbe68293a9dae9204db46836acfed41db6cae4f
followsclosely <mlavwilson@gmail.com>
2016-06-10 13:49:33 -0400

    Added better test coverage to OraclePagingSupport

--- src/main/java/com/pelletier/valuelist/paging/OraclePagingSupport.java
+++ src/main/java/com/pelletier/valuelist/paging/OraclePagingSupport.java
@@ -4,32 +4,36 @@ import com.pelletier.valuelist.PagingInfo;
 
 /**
  * 
- * 	Provides pagination support for Oracle Databases
+ * Provides pagination support for Oracle Databases
  * 
- *  Pre and post SQL can be injected, note that post SQL must have place-holders pageNumber and numberPerPage
+ * Pre and post SQL can be injected, note that post SQL must have place-holders
+ * pageNumber and numberPerPage
  * 
- *  Note: This hasn't been tested.
+ * Note: In Oracle ROWNUM is 1 based.
+ * Note: This hasn't been tested.
  * 
  * 
  * @author Ryan Pelletier
  *
  */
 
-
-public class OraclePagingSupport implements PagingSupport {
+public class OraclePagingSupport implements PagingSupport
+{
 
 	@Override
-	public String getCountQuery(String query) {
+	public String getCountQuery(String query)
+	{
 		return "SELECT count(*) FROM (" + query + ")";
 	}
 
-		
-	//I think this query string can be simplified, but I would like to double check with Matt.
 	@Override
-	public String getPagedQuery(String query, PagingInfo pagingInfo) {
-    	return "SELECT * FROM (SELECT INNER.*, ROWNUM as RECORDNUM FROM (" + 
-				query + 
-				String.format(") INNER ) WRAPPED WHERE WRAPPED.RECORDNUM BETWEEN ((%1$s-1)*%2$s+1) AND (%1$s)*%2$s", new Integer(pagingInfo.getPage()).toString(),new Integer(pagingInfo.getNumberPerPage()).toString());
+	public String getPagedQuery(String query, PagingInfo pagingInfo)
+	{
+		return "SELECT * FROM (SELECT INNER.*, ROWNUM as RECORDNUM FROM (" + query + ") INNER ) WRAPPED " + 
+				"WHERE WRAPPED.RECORDNUM BETWEEN "+
+				(((pagingInfo.getPage() - 1) * pagingInfo.getNumberPerPage()) + 1)  + 
+				" AND " + 
+				(pagingInfo.getPage()) * pagingInfo.getNumberPerPage();
 	}
 
 }
