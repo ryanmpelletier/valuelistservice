ed5da6ff32122e793c2792ee3cad0d89f9b506a8
Pelletier <578993@bah.com>
2016-06-17 10:27:57 -0400

    Have controller return error HTTP response, and catch ConversionExceptions only.

--- src/main/java/com/pelletier/ValueListServiceController.java
+++ src/main/java/com/pelletier/ValueListServiceController.java
@@ -8,11 +8,13 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 import com.pelletier.valuelist.PagingInfo;
 import com.pelletier.valuelist.ValueListService;
@@ -72,8 +74,9 @@ public class ValueListServiceController {
         return valueListService.getValuesList((String) params.get("valueListQuery"), params, pagingInfo);
     }
     
-    @ExceptionHandler(RuntimeException.class)
+    @ExceptionHandler(ConversionException.class)
     @ResponseBody
+    @ResponseStatus(value = HttpStatus.BAD_REQUEST)
     public List<ErrorInfo> exceptionHandler(Exception exception){
         return ((ConversionException) exception).getErrorInfos();
     }
