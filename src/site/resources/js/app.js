702e1d486f526f29e29534129a5c8fbb4c9f9552
Pelletier <578993@bah.com>
2016-06-23 17:51:14 -0400

    Update webpage with simple valuelist.js example.

--- src/site/resources/js/app.js
+++ src/site/resources/js/app.js
@@ -5,15 +5,5 @@
 var app = angular.module('ValueListPageManager',[]);
 
 app.controller('PageManagerController', function($scope){
-	$scope.greeting = "hello";
-	$scope.pageNames = ['ProjectOverview','Interface Overview','Using Default Implementations','Paging Results','Complex Queries','Configuring the AdapterConversionService','Further Extension','Using in Spring MVC'];	
 	$scope.currentPageName = "ProjectOverview";
-	console.log("currentPageName is " + $scope.currentPageName);
-	
-	$scope.setInterfaceOverview = function(){
-		console.log("currentPageName is " + $scope.currentPageName);
-		$scope.currentPageName = "ProjectOverview";
-		console.log("set new page to Interface Overview");
-	};
-	
 });
